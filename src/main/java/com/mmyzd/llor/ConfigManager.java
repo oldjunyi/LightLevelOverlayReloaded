8f109c0fa2e165645e3d5a7229d3af1416030e69
oldjunyi <archon.jk@hotmail.com>
2016-03-18 23:20:57 +0800

    Update ingame config support

--- src/main/java/com/mmyzd/llor/ConfigManager.java
+++ src/main/java/com/mmyzd/llor/ConfigManager.java
@@ -32,20 +32,20 @@ public class ConfigManager {
 		file.load();
 		useSkyLight = file.get("general", "useSkyLight", false);
 		useSkyLight.comment = "If set to true, the sunlight/moonlight will be counted in light level. (default: false)";
-		useSkyLight.set(useSkyLight.getBoolean(false));
 		overlayType = file.get("general", "overlayType", 0);
 		overlayType.comment = "0 - normal, 1 - only shows dangerous (red) area, 2 - only shows safe (green) area. (default: 0)";
-		overlayType.set(Math.min(Math.max(overlayType.getInt(0), 0), 2));
 		chunkRadius = file.get("general", "chunkRadius", 3);
 		chunkRadius.comment = "The distance (in chunks) of rendering radius. (default: 3)";
-		chunkRadius.set(Math.min(Math.max(chunkRadius.getInt(3), 1), 9));
 		pollingInterval = file.get("general", "pollingInterval", 200);
 		pollingInterval.comment = "The update interval (in milliseconds) of light level. Farther chunks update less frequently. (default: 200)";
-		pollingInterval.set(Math.min(Math.max(pollingInterval.getInt(200), 10), 60000));
-		file.save();
+		update();
 	}
 	
 	public void update() {
+		useSkyLight.set(useSkyLight.getBoolean(false));
+		overlayType.set(Math.min(Math.max(overlayType.getInt(0), 0), 2));
+		chunkRadius.set(Math.min(Math.max(chunkRadius.getInt(3), 1), 9));
+		pollingInterval.set(Math.min(Math.max(pollingInterval.getInt(200), 10), 60000));
 		file.save();
 	}
 	
