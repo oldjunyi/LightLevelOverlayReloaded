fab5aebd98de8b679508f4777be468aa395e272c
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-07-10 10:48:33 +1000

    Update to 1.14.3

--- src/main/java/com/mmyzd/llor/util/WeakDataAccessor.java
+++ src/main/java/com/mmyzd/llor/util/WeakDataAccessor.java
@@ -8,7 +8,7 @@ public class WeakDataAccessor<T> {
   private WeakReference<T> reference;
 
   public WeakDataAccessor(T instance) {
-    this.reference = new WeakReference<T>(instance);
+    this.reference = new WeakReference<>(instance);
   }
 
   public void clear() {
